5c7b88a4beab6e8d31e85427a910f970faa8dcca
TerrenceChao <rfvbnjuyt@gmail.com>
2017-06-13 16:13:07 +0800

    Fix bug of store controller:'createStore'

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,7 +11,7 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-    const sotre = await (new Store(req.body)).save();
+    const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 } 
